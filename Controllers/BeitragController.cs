ef84c00530ab410182a89c60e6dd1a2feb15cf46
Felix Jakob <felixjakob04@gmx.de>
2026-01-27 09:25:48 +0100

    Beitragsseite hinzugefügt

---
+++ Controllers/BeitragController.cs
@@ -0,0 +1,19 @@
+﻿using Die1Er_Projektarbeit.Data;
+using Microsoft.AspNetCore.Mvc;
+using Die1Er_Projektarbeit.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Die1Er_Projektarbeit.Controllers
+{
+    public class BeitragController(ApplicationDbContext context) : Controller
+    {
+        public readonly ApplicationDbContext _context = context;
+
+        public IActionResult Beitrag(int id)
+        {
+            Beitrag beitrag = _context.Beitrag.Include(x => x.Autor)
+                .Where(x => x.Id == id).FirstOrDefault();
+            return View(beitrag);
+        }
+    }
+}
